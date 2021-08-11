b7de0e5bad911461cf13c106e50941b4ed5455e6
krishnacnambiar <64839459+krishnacnambiar@users.noreply.github.com>
2021-08-11 10:00:29 +0530

    Update Employee.java

--- srv/src/main/java/com/ey/ttt/handlers/Employee.java
+++ srv/src/main/java/com/ey/ttt/handlers/Employee.java
@@ -1,5 +1,7 @@
 package com.ey.ttt.handlers;
 
+import java.util.List;
+
 import com.sap.cds.services.ErrorStatus;
 import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.services.EventContext;
@@ -11,17 +13,17 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 
 import org.springframework.stereotype.Component;
 
-import cds.gen.db.EmployeeCounselorRelationship;
+import cds.gen.employeeservice.EmployeeCounselorRelation;
 import cds.gen.employeeservice.EmployeeService_;
-import io.vavr.collection.List;
+
 
 
 @Component
 @ServiceName(EmployeeService_.CDS_NAME)
 public class Employee implements EventHandler{
     @Before(event = {CdsService.EVENT_CREATE, CdsService.EVENT_UPDATE, CdsService.EVENT_UPSERT}, entity = EmployeeService_.CDS_NAME)
-    public void validateCounselor(List<EmployeeCounselorRelationship> datalist, EventContext eventContext){
-        for (EmployeeCounselorRelationship employeeCounselorRelationship : datalist) {
+    public void validateCounselor(List<EmployeeCounselorRelation> datalist, EventContext eventContext){
+        for (EmployeeCounselorRelation employeeCounselorRelationship : datalist) {
             if(employeeCounselorRelationship.getEmployeeId() == employeeCounselorRelationship.getCounselorId()){
                 throw new ServiceException(ErrorStatuses.NOT_ACCEPTABLE, "Manager and Counselor cannot be the same Person");
             }
